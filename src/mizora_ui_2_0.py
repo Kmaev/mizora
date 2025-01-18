c48efe1dfd187412043c572f064bf58dd65bf28e
Kristina Maevskaya <maevskayakrisfx@gmail.com>
2025-01-19 10:52:04 +1100

    Add Houdini Syntax Highlighting for Code Editor
    Introduced PythonSyntaxHighlighter class to provide syntax highlighting for the code editor.
    
    Added support for highlighting:
    Keywords: Includes int, float, string, vector, None, and control flow keywords like break, continue, and pass.
    Operators: Includes if, for, while, foreach, else, and else if.
    Attributes: Highlighted patterns starting with @.
    Functions: Highlighted VEX function calls using a regex pattern to match function names followed by parentheses.
    Strings: Added support for highlighting both single and double-quoted strings.
    Comments: Highlighted single-line comments starting with //.

--- src/mizora_ui_2_0.py
+++ src/mizora_ui_2_0.py
@@ -7,9 +7,6 @@ import _houdini
 
 reload(_houdini)
 
-thumbnail_path = '/Users/kmaev/Documents/hou_dev/arcane_tower/mizora/mizora/resources/mizora_thumbnail.JPG'
-
-
 class Assembler(QtWidgets.QDialog):
     def __init__(self, parent=None):
         super(Assembler, self).__init__(parent=parent)
@@ -22,13 +19,12 @@ class Assembler(QtWidgets.QDialog):
         self.context_line = QtWidgets.QLineEdit()
         self.context_line.setPlaceholderText(
             "Write parent node here: '/obj/my_geometry'")
-
         self.central_layout.addWidget(self.context_line)
+
         # Set completer to context line
         self.set_completer()
 
         # Search Group Widgets
-
         self.search_grp = QtWidgets.QGroupBox()
         self.search_grp_layout = QtWidgets.QHBoxLayout()
         self.search_grp.setLayout(self.search_grp_layout)
@@ -38,7 +34,6 @@ class Assembler(QtWidgets.QDialog):
         self.search_line = QtWidgets.QLineEdit()
         self.search_line.setPlaceholderText('Search variable')
         self.search_grp_layout.addWidget(self.search_line)
-
         self.search_btn = QtWidgets.QPushButton("Search")
         self.search_grp_layout.addWidget(self.search_btn)
 
@@ -49,16 +44,17 @@ class Assembler(QtWidgets.QDialog):
         self.central_layout.addWidget(self.edit_grp)
 
         # Add Search Output QList
-
         self.search_result_list = QtWidgets.QListWidget()
         self.edit_grp_layout.addWidget(self.search_result_list)
 
         # Add Code Edit QTextEdit
         self.code_edit = QtWidgets.QTextEdit()
+        font = QtGui.QFont("Menlo", 14, QtGui.QFont.Bold)  # Font: Arial, Size: 14, Bold and Italic
+        self.code_edit.setCurrentFont(font)
         self.edit_grp_layout.addWidget(self.code_edit)
+        self.highlighter = PythonSyntaxHighlighter(self.code_edit.document())
 
         # Add Rename Group (part of the Edit Layout)
-
         self.rename_grp = QtWidgets.QGroupBox()
         self.rename_grp_layout = QtWidgets.QVBoxLayout()
         self.rename_grp_layout.setAlignment(QtCore.Qt.AlignTop)
@@ -66,33 +62,24 @@ class Assembler(QtWidgets.QDialog):
         self.edit_grp_layout.addWidget(self.rename_grp)
 
         # Add New Variable Name QLine
-
         self.new_name_line = QtWidgets.QLineEdit()
         self.new_name_line.setPlaceholderText('Rename to:')
         self.rename_grp_layout.addWidget(self.new_name_line)
 
         # Add Rename All QCheckBox
-
         self.rename_all_check = QtWidgets.QCheckBox("Rename all")
         self.rename_grp_layout.addWidget(self.rename_all_check)
 
         # Add Rename Button QPushButton
-
         self.rename_btn = QtWidgets.QPushButton('Rename')
         self.rename_grp_layout.addWidget(self.rename_btn)
 
         # Add Apply Button QPushButton
-
         self.apply_btn = QtWidgets.QPushButton('Apply')
         self.rename_grp_layout.addWidget(self.apply_btn)
 
-        # DEBUG SECTION
-        self.test = QtWidgets.QPushButton('Debug')
-        self.central_layout.addWidget(self.test)
-        # END OF DEBUG SECTION
-
         # Buttons connection
-        self.test.clicked.connect(self.get_current_context)
+        # self.test.clicked.connect(self.get_current_context)
         self.search_btn.clicked.connect(self.on_search_btn_executed)
         self.search_result_list.itemSelectionChanged.connect(self.on_list_item_changed)
         self.rename_btn.clicked.connect(self.on_rename_btn_executed)
@@ -101,19 +88,29 @@ class Assembler(QtWidgets.QDialog):
         self.setLayout(self.central_layout)
 
         self.var_occurrences_map = {}
+        self.prev_selected = None
 
         # ADD STYLE SHEET
         script_dir = os.path.dirname(__file__)
         resources_path = os.path.join(script_dir, "..", "resources")
-
         resources_path = os.path.normpath(resources_path)
 
         with open(os.path.join(resources_path, "style_hou.qss"), 'r') as f:
             self.setStyleSheet(f.read())
 
-    # CONTEXT LINE FUNCTIONS
-    #  Context line completer
+        # CONTEXT LINE FUNCTIONS
+        # Context line completer
+
     def set_completer(self):
+        """
+        Sets up an auto-completer for the context line widget.
+
+        This function populates a QCompleter with paths of Houdini nodes
+        of specific types (`subnet` and `geo`) located within the root node ('/').
+        The completer is then attached to the `context_line` widget, enabling
+        auto-completion for valid node paths.
+        """
+
         allowed = ('subnet', 'geo')
         nodes = sorted([x.path() for x in hou.node('/').allSubChildren(True, False)
                         if x.type().name() in allowed])
@@ -121,17 +118,39 @@ class Assembler(QtWidgets.QDialog):
         completer.popup().setObjectName('completer')
         self.context_line.setCompleter(completer)
 
-    #   Return currently selected houdini context
-    def get_current_context(self):  # Do this function needed or it could be sources directly from _houdini.py
+        # Return currently selected houdini context
+
+    def get_current_context(self) -> list:
+        """
+        Retrieves the current search context based on the input in the context line.
+        """
         return _houdini.get_hou_search_context(self.context_line.text())
 
     def on_search_btn_executed(self):
+        """
+        Executes the search operation when the search button is clicked.
+
+        If the search field is not empty, it populates the search results list and updates related components.
+        Otherwise, it displays an error message and raises a ValueError.
+        """
         self.search_result_list.clear()
-        self.var_occurrences_map = {}
-        self.populate_search_result_list()
-        self.search_result_list.item(0).setSelected(True)
-        self.search_result_list.setCurrentRow(0)
-        self.populate_code_editor()
+        self.code_edit.clear()
+        if self.search_line.text() != '':
+            self.var_occurrences_map = {}
+            self.populate_search_result_list()
+            if self.search_result_list.count() > 0:
+                self.search_result_list.item(0).setSelected(True)
+                self.search_result_list.setCurrentRow(0)
+                self.populate_code_editor()
+                self.prev_selected = self.get_current_key()
+        else:
+            hou.ui.displayMessage(
+                text=f"Search field cannot be empty. Please enter a variable to search.",
+                buttons=('OK',),
+                title="Error",
+                severity=hou.severityType.Error
+            )
+            raise ValueError("Search field is empty")
 
     def populate_search_result_list(self) -> None:
         """
@@ -139,27 +158,32 @@ class Assembler(QtWidgets.QDialog):
         """
         self.search_result_list.clear()
         current_context = _houdini.get_hou_search_context(self.context_line.text())
+
         self.var_occurrences_map = _houdini.find_variable_occurrences(current_context, self.search_line.text())
+        if self.var_occurrences_map:
+            # Populate the list widget
+            for item, snippet in self.var_occurrences_map.items():
+                list_item = QtWidgets.QListWidgetItem(item.path())
+                list_item.setData(QtCore.Qt.UserRole, snippet)
+                self.search_result_list.addItem(list_item)
+        else:
+            hou.ui.displayMessage(
+                text=f"Variable Not Found\nThe variable '{self.search_line.text()}' does not exist or is not defined. "
+                     f"Please ensure you have entered the correct name and try again.",
+                buttons=('OK',),
+                title="Error",
+                severity=hou.severityType.Error
+            )
+            raise ValueError("Variable Not Found")
 
-        # Populate the list widget
-        for item, snippet in self.var_occurrences_map.items():
-            list_item = QtWidgets.QListWidgetItem(item.path())
-            list_item.setData(QtCore.Qt.UserRole, snippet)
-            self.search_result_list.addItem(list_item)
-
-        # Debug
-        """if self.search_result_list.count() > 0:
-            first_item = self.search_result_list.item(0)
-            print(first_item.data(QtCore.Qt.UserRole))"""
-
-    def get_selection(self, widget):
+    def get_selection(self, widget: object) -> object:
         """
         Get the currently selected QListWidgetItem from the widget.
         """
         selected = widget.selectedItems()
         return selected[0] if selected else None
 
-    def get_current_key(self):
+    def get_current_key(self) -> str:
         """
         Retrieves the key from `self.var_occurrences_map` corresponding to the currently selected item
         in the search results list widget.
@@ -168,19 +192,22 @@ class Assembler(QtWidgets.QDialog):
         - Matches the selected item's text with the `path()` of keys in `self.var_occurrences_map`.
         - If no match is found, prints an error message and returns None.
 
-        :return: The matching key (`node_key`) or None if not found.
         """
         selected_item = self.get_selection(self.search_result_list)
         if selected_item:
 
             # current_code = selected_item.data(QtCore.Qt.UserRole)
-            node_key = next((key for key in self.var_occurrences_map if key.path() == selected_item.text()), None)
+            try:
+                node_key = next(key for key in self.var_occurrences_map if key.path() == selected_item.text())
+            except StopIteration:
+                raise ValueError("No matching key found in var_occurrences_map for the selected item.")
+
             if not node_key:
                 print("Error: Selected node not found in var_occurrences_map")
                 return
         return node_key
 
-    def get_current_code(self):
+    def get_current_code(self) -> str:
         """
         Retrieves the current code associated with the currently selected item in the list widget.
         :return: The current code as a string.
@@ -198,8 +225,10 @@ class Assembler(QtWidgets.QDialog):
         """
 
         current_code = self.get_current_code()
+
         if current_code:
             self.code_edit.setPlainText(current_code)
+
         else:
 
             self.code_edit.setPlainText("")
@@ -207,9 +236,15 @@ class Assembler(QtWidgets.QDialog):
     def on_list_item_changed(self):
         """
         Populates the code editor when the selection in the search results list changes.
-        :return: None
         """
+        editted_code = self.code_edit.toPlainText()
+
+        # print(editted_code)
+
         self.populate_code_editor()
+        self.var_occurrences_map[self.prev_selected] = editted_code
+        if self.get_selection(self.search_result_list):
+            self.prev_selected = self.get_current_key()
 
     def on_rename_btn_executed(self):
         """
@@ -220,7 +255,6 @@ class Assembler(QtWidgets.QDialog):
         - Updates the list widget and code editor to reflect the changes.
         - If 'Rename All' is checked, applies renaming to all occurrences. Otherwise, moves
         the selection to the next occurrence in the list.
-        :return: None
         """
         new_name = self.new_name_line.text()
         searched_name = self.search_line.text()
@@ -239,14 +273,22 @@ class Assembler(QtWidgets.QDialog):
         current_index = self.search_result_list.currentRow()
         if self.rename_all_check.isChecked():
             self.rename_all()
+            hou.ui.displayMessage(
+                text=f"'{self.search_line.text()}' has been renamed to '{self.new_name_line.text()}'",
+                buttons=('OK',),
+                severity=hou.severityType.Message
+            )
         else:
-            if current_index < len(self.var_occurrences_map.keys()) - 1:
-                print(current_index)
+            # if current_index < len(self.var_occurrences_map.keys()) - 1:
+            if current_index < self.search_result_list.count() - 1:
                 self.search_result_list.setCurrentRow(current_index + 1)
                 self.search_result_list.item(current_index + 1).setSelected(True)
             else:
                 self.search_result_list.setCurrentRow(0)
                 self.search_result_list.item(0).setSelected(True)
+
+
+        # print(f"{self.var_occurrences_map} - On rename executed")
 
     def rename_all(self):
         """
@@ -255,8 +297,6 @@ class Assembler(QtWidgets.QDialog):
         - Iterates through each key-value pair in the variable occurrences map.
         - Replaces the old variable name with the new one in the code.
         - Updates the dictionary with the modified code for each node.
-
-        :return: None
         """
         new_name = self.new_name_line.text()
         searched_name = self.search_line.text()
@@ -271,12 +311,89 @@ class Assembler(QtWidgets.QDialog):
         Saves all edited code into the corresponding Wrangle nodes.
         :return: None
         """
+
+        editted_code = self.code_edit.toPlainText()
+
+        self.var_occurrences_map[self.get_current_key()] = editted_code
         for node_key, current_code in self.var_occurrences_map.items():
-            node_key.parm('snippet').set(current_code)
+            if node_key:
+                node_key.parm('snippet').set(current_code)
 
         self.new_name_line.clear()
         self.rename_all_check.setChecked(False)
         self.search_line.clear()
+
+
+class PythonSyntaxHighlighter(QtGui.QSyntaxHighlighter):
+    def __init__(self, document):
+        super().__init__(document)
+        self.highlighting_rules = []
+
+        keyword_format = QtGui.QTextCharFormat()
+        keyword_format.setForeground(QtGui.QColor(137, 198, 219))  # Blue-ish
+        keyword_format.setFontWeight(QtGui.QFont.Bold)
+
+        at_keyword_format = QtGui.QTextCharFormat()
+        at_keyword_format.setForeground(QtGui.QColor(211, 207, 156))  # Yellow-ish
+        at_keyword_format.setFontWeight(QtGui.QFont.Bold)
+
+        string_format = QtGui.QTextCharFormat()
+        string_format.setForeground(QtGui.QColor(121, 169, 120))  # Green
+
+        comment_format = QtGui.QTextCharFormat()
+        comment_format.setForeground(QtGui.QColor(122, 126, 132))  # Gray
+        # comment_format.setFontItalic(True)
+
+        operator_format = QtGui.QTextCharFormat()
+        operator_format.setForeground(QtGui.QColor(99, 148, 185))  # Blue
+        operator_format.setFontWeight(QtGui.QFont.Bold)
+
+        function_format = QtGui.QTextCharFormat()
+        function_format.setForeground(QtGui.QColor(131, 131, 185))  # Purple
+        function_format.setFontWeight(QtGui.QFont.Bold)
+
+        keywords = [
+            "int\s", "float\s", "string\s", "vector\s", "None",
+            "break", "continue", "pass"
+        ]
+
+        operators = [
+            "if", "for", "while", "foreach",
+            "else", "else if"
+        ]
+
+        at_keywords = [
+            "@[a-zA-Z0-9_]+", "[a-zA-Z]@[a-zA-Z0-9_]+", "[a-zA-Z]\[\]@[a-zA-Z0-9_]+"
+        ]
+
+        vex_functions = [
+            "(?<!\\w)([a-zA-Z_][a-zA-Z0-9_]*)\\s*(?=\\()"
+        ]
+
+        for function in vex_functions:
+            self.highlighting_rules.append((QtCore.QRegularExpression(function), function_format))
+
+        for operator in operators:
+            self.highlighting_rules.append((QtCore.QRegularExpression(operator), operator_format))
+
+        for at_keyword in at_keywords:
+            self.highlighting_rules.append((QtCore.QRegularExpression(at_keyword), at_keyword_format))
+
+        for keyword in keywords:
+            self.highlighting_rules.append((QtCore.QRegularExpression(keyword), keyword_format))
+
+        self.highlighting_rules.append((QtCore.QRegularExpression("\".*\""), string_format))
+        self.highlighting_rules.append((QtCore.QRegularExpression("'.*'"), string_format))
+        self.highlighting_rules.append((QtCore.QRegularExpression("//[^\n]*"), comment_format))
+
+    def highlightBlock(self, text):
+        for pattern, text_format in self.highlighting_rules:
+            match_iterator = pattern.globalMatch(text)
+            while match_iterator.hasNext():
+                match = match_iterator.next()
+                start = match.capturedStart()
+                length = match.capturedLength()
+                self.setFormat(start, length, text_format)
 
 
 dialog = None
