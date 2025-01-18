245e0b76c981fe42b354aff28b3f26169d874a69
Kristina Maevskaya <maevskayakrisfx@gmail.com>
2025-01-19 10:46:45 +1100

    Enhance path validation and variable search functionality in Houdini script
    Improved logic for matching Houdini node paths:
    Handled segments with * by checking children for matching names.
    Collected all possible roots that match the given pattern.
    
    Updated find_variable_occurrences:
    Used regex to match variables in attribwrangle node snippets.

--- src/_houdini.py
+++ src/_houdini.py
@@ -52,43 +52,49 @@ def get_hou_search_context(path: str) -> list:
             severity=hou.severityType.Error
         )
         raise ValueError("The path cannot be empty.")
-
-    hou_contexts_list = []
-
-    # Create context list based on the path
-    # Wildcard case
-    if path.endswith("*"):
-
-        hou_contexts_list = parse_wildcard_path(path)
-
-    else:
-        # Append directly if no wildcard used
-        node = hou.node(path)
-        if node:
-            hou_contexts_list.append(node)
-
-        else:
-            hou.ui.displayMessage(
-                text=f"Invalid Houdini node path: {path}",
-                buttons=('OK',),
-                title="Error",
-                severity=hou.severityType.Error
-            )
-            raise RuntimeError(f"Invalid Houdini node path: {path}")
-
-    # RETURN
-    if hou_contexts_list:
-        return hou_contexts_list
-
-        # Error message if no valid context is found
-    else:
+    if not path.startswith("/"):
         hou.ui.displayMessage(
-            text='No valid context was selected. Please select a valid context. For example: "/obj/my_geometry".',
+            text=f"Invalid Houdini node path: {path}",
             buttons=('OK',),
             title="Error",
             severity=hou.severityType.Error
         )
-        raise RuntimeError('No valid context was selected')
+        raise ValueError(f"Invalid Houdini node path: {path} Pattern must start with '/'")
+    segments = path.split("/")
+
+    # Assume the first candidate is always "/"
+    # Because we can find multiple roots that might match the pattern,
+    # we need to collect the list of all possible roots that match it.
+    candidates = [hou.node("/")]
+    new_candidates = []
+    path_list = []
+
+    while segments:
+        segment = segments.pop(0)
+
+        # Empty strings mean double or leading '/' and we can ignore them
+        if not segment:
+            continue
+
+        for candidate in list(candidates):
+            # For each candidate if there's a "*" in the segment, we need to check
+            # that every individual child matches the name we want to find.
+            if "*" in segment:
+                for child in candidate.children():
+                    if hou.text.patternMatch(segment, child.name()):
+                        new_candidates.append(child)
+            else:
+                node = candidate.node(segment)
+                if node:
+                    new_candidates.append(node)
+
+        # Reset the candidates
+        candidates = new_candidates
+        new_candidates = []
+
+    for candidate in candidates:
+        path_list.append(hou.node(candidate.path()))
+    return path_list
 
 
 def find_variable_occurrences(context: list, searched_var: str) -> dict:
@@ -101,8 +107,7 @@ def find_variable_occurrences(context: list, searched_var: str) -> dict:
     """
     nodes = {}
 
-    pattern = re.compile(rf'\b{re.escape(searched_var)}\b')
-
+    pattern = re.compile(rf'{re.escape(searched_var)}')
     for geo in context:
         for child in geo.children():
             # Check if the child is attribwrangle
@@ -113,6 +118,4 @@ def find_variable_occurrences(context: list, searched_var: str) -> dict:
 
             # Recursive check
             nodes.update(find_variable_occurrences([child], searched_var))
-
     return nodes
-#TODO output message is the variable was not founded
